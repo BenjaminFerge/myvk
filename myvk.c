b2d288ffa7504883a56621f6824830fb8db3bbe4
Benjámin J. Ferge <benjamin.ferge@protonmail.com>
2020-05-07 07:38:07 +0200

    Create an instance

--- myvk.c
+++ myvk.c
@@ -1,5 +1,35 @@
 #include "myvk.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+void create_inst(myvk_ctx* ctx)
+{
+    VkApplicationInfo app;
+    app.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app.pApplicationName = "myvk";
+    app.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app.pEngineName = "myvk";
+    app.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app.apiVersion = VK_API_VERSION_1_2;
+
+    VkInstanceCreateInfo create;
+    create.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    create.pApplicationInfo = &app;
+
+    uint32_t extc = 0;
+    const char** exts;
+    exts = glfwGetRequiredInstanceExtensions(&extc);
+    create.enabledExtensionCount = extc;
+    create.ppEnabledExtensionNames = exts;
+    create.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&create, NULL, &ctx->inst);
+
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "Cannot create a Vulkan instance\n");
+        exit(1);
+    }
+}
 
 void init_window(myvk_ctx* ctx)
 {
@@ -14,6 +44,7 @@ myvk_ctx* myvk_init()
     myvk_ctx* ctx = malloc(sizeof(myvk_ctx));
     ctx->exit = false;
     init_window(ctx);
+    create_inst(ctx);
     return ctx;
 }
 
@@ -31,6 +62,7 @@ void myvk_end(myvk_ctx* ctx)
 
 void myvk_free(myvk_ctx* ctx)
 {
+    vkDestroyInstance(ctx->inst, NULL);
     glfwDestroyWindow(ctx->window);
     glfwTerminate();
     free(ctx);
--- myvk.h
+++ myvk.h
@@ -8,12 +8,13 @@
 typedef struct myvk_ctx {
     GLFWwindow* window;
     bool exit;
+    VkInstance inst;
 } myvk_ctx;
 
-myvk_ctx *myvk_init();
+myvk_ctx* myvk_init();
 
-void myvk_free(myvk_ctx *ctx);
+void myvk_free(myvk_ctx* ctx);
 void myvk_begin(myvk_ctx* ctx);
 void myvk_end(myvk_ctx* ctx);
 
-#endif //MYVK_MYVK_H
+#endif // MYVK_MYVK_H
